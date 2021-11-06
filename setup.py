e9bd8c67c5350071c792520f1e99f2c55e0dd525
calys <sylvain.reboux_github@m4x.org>
2021-11-06 09:40:00 +0100

    define drnukebean package from toplevel src

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name = 'drnukebean',
       version = '0.1',
       description = "Dr Nukes's beancount arsenal",
-      packages = ['src.drnukebean.importer'],
+      packages = ['drnukebean'],
+      package_dir = {'drnukebean': 'src'},
       zip_safe = False)
