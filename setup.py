c15c7b90a89823deca1ea285162cbde6652419e6
calys <sylvain.reboux_github@m4x.org>
2021-12-01 21:40:23 +0100

    package_dir in setuptool

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name = 'drnukebean',
       version = '0.1',
       description = "Dr Nukes's beancount arsenal",
       packages = find_packages(include=['src/drnukebean']),
-      package_dir = {'drnukebean': 'src'},
+      package_dir = {'drnukebean': 'src/drnukebean'},
       zip_safe = False)
