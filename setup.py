2f0251b54f8bb3066edfacdae334f5f38f4cdd74
calys <sylvain.reboux_github@m4x.org>
2021-12-01 22:13:34 +0100

    removing...

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name = 'drnukebean',
       description = "Dr Nukes's beancount arsenal",
       packages = find_packages(
             where='src',
-            include=['drnukebean.importer'],
+            include=['drnukebean'],
       ),
       package_dir = {'': 'src'},
       zip_safe = False)
